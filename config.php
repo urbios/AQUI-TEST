dc782bda77099bed7960b20c030663853ebdaf3a
urbios <xinito92@hotmail.com>
2014-12-29 11:26:50 +0100

    hola
    ke ase

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	$database_host = "mysql4.000webhost.com";
+	$database_username = "a4560338_juan";
+	$database_password = "warcraft23";
+	$database_name = "db";
+	
+	$START_GOLD = 1000;
+	$MAX_BAZAAR = 5;
+?>
